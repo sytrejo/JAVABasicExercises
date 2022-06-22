af7f9261b9f5b28ef66f16086d91b94a4328509c
Silvia Trejo <s.y.trejo26@gmail.com>
2022-06-22 19:15:59 -0400

    completing problems 11 to 30...

--- JAVABasicExercises/src/basicPart1/basic11to20.java
+++ JAVABasicExercises/src/basicPart1/basic11to20.java
@@ -74,6 +74,16 @@ public class basic11to20 {
 	       d = c - d;
 	       c = c - d;
 	       System.out.println("After swapping : a, b = "+c+", "+ + d);
+	       
+	       
+	       //Java program that checks whether JAVA is installed on your computer
+	       System.out.println("\nJava Version: "+System.getProperty("java.version"));
+	       System.out.println("Java Runtime Version: "+System.getProperty("java.runtime.version"));
+	       System.out.println("Java Home: "+System.getProperty("java.home"));
+	       System.out.println("Java Vendor: "+System.getProperty("java.vendor"));
+	       System.out.println("Java Vendor URL: "+System.getProperty("java.vendor.url"));
+	       System.out.println("Java Class Path: "+System.getProperty("java.class.path")+"\n");
+	       
 	        
 	    }
 
